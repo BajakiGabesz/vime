9453711b74b07bfe8746d715340ac784e87f01f7
Gábor Bajáki <81427020+BajakiGabesz@users.noreply.github.com>
2023-03-27 23:59:30 +0200

    Minor fixes.

--- packages/core/src/components/providers/hls/hls.tsx
+++ packages/core/src/components/providers/hls/hls.tsx
@@ -210,7 +210,7 @@ export class HLS implements MediaFileProvider {
   }
 
   private levelToPlaybackQuality(level: any) {
-    return level === -1 ? 'Auto' : `${level.bitrate}p`;
+    return level === -1 ? 'Auto' : `${level.bitrate}`;
   }
 
   private findLevelIndexFromQuality(quality: PlayerProps['playbackQuality']) {
